ec3ca75127ff91cd192972d331b01cf299ee5e0b
Astrid Mongi <12410384@student.unklab.ac.id>
2025-11-04 10:32:53 +0800

    inisialisasi evolution

---
+++ src/components/header/generations/gen2.js
@@ -0,0 +1,2 @@
+const Gen2 = ({ text }) => <div className="bg-white p-2 rounded">{text}</div>;
+export default Gen2;
---
+++ src/components/header/generations/gen3.js
@@ -0,0 +1,2 @@
+const Gen3 = ({ text }) => <div className="bg-white p-2 rounded">{text}</div>;
+export default Gen3;
